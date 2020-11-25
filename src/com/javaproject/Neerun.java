123fb75235674db870e318c0992e9a178284c055
neerun <74941082+neerun@users.noreply.github.com>
2020-11-25 14:15:55 -0500

    inital commit

---
+++ src/com/javaproject/Neerun.java
@@ -0,0 +1,14 @@
+package com.javaproject;
+
+public class Neerun {
+	
+	public void neerun() {
+		System.out.println("My name is neeran gurung.");
+}
+
+	public static void main(String[] args) {
+		Neerun obj = new Neerun();
+		obj.neerun();
+		
+	}
+}
